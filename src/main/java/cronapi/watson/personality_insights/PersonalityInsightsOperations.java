794777389981b99af24a1de22501d5e5afc2999c
Michel Moreira <michel@michelmoreira.eti.br>
2018-02-21 13:26:04 -0300

    Initial commit

---
+++ src/main/java/cronapi/watson/personality_insights/PersonalityInsightsOperations.java
@@ -0,0 +1,34 @@
+package cronapi.watson.personality_insights;
+
+import com.ibm.watson.developer_cloud.personality_insights.v3.PersonalityInsights;
+import com.ibm.watson.developer_cloud.personality_insights.v3.model.Profile;
+import com.ibm.watson.developer_cloud.personality_insights.v3.model.ProfileOptions;
+import cronapi.CronapiMetaData;
+
+import java.util.Map;
+
+@CronapiMetaData
+public final class PersonalityInsightsOperations {
+
+  @CronapiMetaData
+  public static Profile profile(String versionDate, String username, String password, String endPoint,
+                                Map<String, String> headers, ProfileOptions profileOptions) {
+    PersonalityInsights service = new PersonalityInsights(versionDate);
+    service.setUsernameAndPassword(username, password);
+    service.setEndPoint(endPoint);
+    service.setDefaultHeaders(headers);
+
+    return service.profile(profileOptions).execute();
+  }
+
+  @CronapiMetaData
+  public static String getProfileAsCSV(String versionDate, String username, String password, String endPoint,
+                                       Map<String, String> headers, ProfileOptions profileOptions, boolean includeHeaders) {
+    PersonalityInsights service = new PersonalityInsights(versionDate);
+    service.setUsernameAndPassword(username, password);
+    service.setEndPoint(endPoint);
+    service.setDefaultHeaders(headers);
+
+    return service.getProfileAsCSV(profileOptions, includeHeaders).execute();
+  }
+}
